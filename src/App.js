728e167f766cd4ae275cb67b09da5291f4610d4a
suat çelik <sc.suatcelik@gmail.com>
2023-11-22 14:59:27 +0300

    önceki ,sıfırla ve geçiş kodları yazıldı

--- src/App.js
+++ src/App.js
@@ -2,22 +2,38 @@ import { useState } from "react";
 import { Switch, Route, NavLink } from "react-router-dom";
 import Movie from "./components/Movie";
 import FavMovie from "./components/FavMovie";
+import { movies } from "./movies";
 
 function App() {
   const [sira, setSira] = useState(0);
   const favMovies = [];
 
   function sonrakiFilm() {
-    setSira(sira + 1);
+    setSira((prevIndex) => (prevIndex + 1) % movies.length);
+  }
+  function oncekiFilm() {
+    setSira((prevIndex) => (prevIndex - 1 + movies.length) % movies.length);
+  }
+  function sıfırla() {
+    setSira(0);
   }
 
   return (
     <div className="wrapper max-w-2xl mx-auto">
       <nav className="flex text-2xl pb-6 pt-8 gap-2 justify-center">
-        <NavLink to="/" exact className="py-3 px-6 " activeClassName="bg-white shadow-sm text-blue-600">
+        <NavLink
+          to="/"
+          exact
+          className="py-3 px-6 "
+          activeClassName="bg-white shadow-sm text-blue-600"
+        >
           Filmler
         </NavLink>
-        <NavLink to="/listem" className="py-3 px-6 " activeClassName="bg-white shadow-sm text-blue-600">
+        <NavLink
+          to="/listem"
+          className="py-3 px-6 "
+          activeClassName="bg-white shadow-sm text-blue-600"
+        >
           Listem
         </NavLink>
       </nav>
@@ -26,6 +42,18 @@ function App() {
           <Movie sira={sira} />
 
           <div className="flex gap-3 justify-end py-3">
+            <button
+              onClick={sıfırla}
+              className="select-none px-4 py-2 border border-red-700 text-red-700 hover:border-red-500 hover:text-blue-500"
+            >
+              Sıfırla
+            </button>
+            <button
+              onClick={oncekiFilm}
+              className="select-none px-4 py-2 border border-blue-700 text-blue-700 hover:border-blue-500 hover:text-blue-500"
+            >
+              Önceki
+            </button>
             <button
               onClick={sonrakiFilm}
               className="select-none px-4 py-2 border border-blue-700 text-blue-700 hover:border-blue-500 hover:text-blue-500"
